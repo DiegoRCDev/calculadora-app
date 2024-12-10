7b6ec8df803a593be78e2324a866e69159c63560
DiegoRCDev <diego.rodriguez.crespo1993@gmail.com>
2024-12-10 17:29:31 +0100

    Parte de proyecto perteneciente al curso Universidad Angular - de cero a experto - Angular 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'calculadora-app';
+  titulo = 'Aplicación Calculadora';
+
+  resultadoPadre!: number;
+
+  procesarResultado(resultado: number) {
+    this.resultadoPadre = resultado;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ResultadoComponent } from './components/resultado/resultado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ResultadoComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
